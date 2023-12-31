97be4c9202dfd3e087736f1aa29166d0a3c173f5
Maria <maria@maria-IdeaPad-3-15IIL05.(none)>
2023-12-31 18:48:08 +0100

    maria

--- controller/ErreserbatutakoLiburuakController.py
+++ controller/ErreserbatutakoLiburuakController.py
@@ -10,21 +10,53 @@ class ErreserbatutakoLiburuakController:
 			cls.__instance = super(ErreserbatutakoLiburuakController, cls).__new__(cls)
 			cls.__instance.__initialized = False
 		return cls.__instance
+		
+		
+	def __init__(self):
+        if not self.__initialized:
+            self.__initialized = True
 
 
 	def bueltatu_liburua(self, hasiData, bukatuData, erabId, bookId):
 		db.delete("DELETE FROM Mailegatu WHERE hasiData = ? AND bukatuData = ? AND erabId = ? AND bookId = ?", (hasiData, bukatuData, erabId, bookId))
 
 
-	def erreserbatu_liburua(self, erabId, bookId, data):
-		db = tools.hash_password(pasahitza)
-		if admin == "true":
-			admin_balioa = 1
-		else:
-			admin_balioa = 0
-		db.insert("INSERT INTO Mailegatu VALUES (NULL, ?, ?, ?, ?)", (izena, emaila, hp, admin_balioa))
+	def erreserbatu_liburua(self, userId, bookId):
+        # Verificar si el usuario ya tiene reservado el libro
+        erreserbatuta_dauka = db.select("SELECT * FROM Mailegatu WHERE userId = ? AND bookId = ?", (userId, bookId))
+
+        if erreserbatuta_dauka:
+            # El usuario ya tiene reservado este libro
+            return False
+        else:
+            # Reservar el libro para el usuario
+            db.insert("INSERT INTO Mailegatu (userId, bookId, hasiData) VALUES (?, ?, CURRENT_TIMESTAMP)", (userId, bookId))
+            return True
 	
 	
+	def search_books(self, title="", author="", limit=6, page=0):
+		count = db.select("""
+				SELECT count() 
+				FROM Book b, Author a 
+				WHERE b.author=a.id 
+					AND b.title LIKE ? 
+					AND a.name LIKE ? 
+		""", (f"%{title}%", f"%{author}%"))[0][0]
+		res = db.select("""
+				SELECT b.* 
+				FROM Book b, Author a 
+				WHERE b.author=a.id 
+					AND b.title LIKE ? 
+					AND a.name LIKE ? 
+				LIMIT ? OFFSET ?
+		""", (f"%{title}%", f"%{author}%", limit, limit*page))
+		books = [
+			Book(b[0],b[1],b[2],b[3],b[4])
+			for b in res
+		]
+		return books, count
+		
+		
 	def jadaMailegatuZuen(self, eraId, libId):
 		zenbakia = db.select("SELECT count(*) FROM Mailegatu M WHERE M.eraId= ? AND M.libId= ? And bukaeraData is not Null", (eraId, libId))
 		if zenbakia[0][0] >=1:
