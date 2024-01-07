e7d3c51743c3f6605329eba48bb42627f3dd1f66
Ainoa <81186158+Ainoaa@users.noreply.github.com>
2024-01-07 23:38:48 +0100

    Update webServer.py

--- controller/webServer.py
+++ controller/webServer.py
@@ -269,6 +269,10 @@ def LagunenGomendioak():
     irakurritako_liburuen_lagunak = gomendatutako_lagunak_liburuekiko
     total_pages_zure_lag_lib = (len(gomendatutako_lagunak_liburuekiko)//4) +1
 
+    #if "eskaera" in request.values:
+    #    User.gehituEskaera(request.values.get("mail"))
+    #elif "eskaera1" in request.values:
+    #    User.gehituEskaera(request.values.get("mail1"))
 
     return render_template('LagunenGomendioak.html', lagunen_lagunak=lagunen_lagunak, current_page_lagunen_lagunak=page_lagunen_lagunak, total_pages_lagunen_lagunak=total_pages_lagunen_lagunak,
                 irakurritako_liburuen_lagunak=irakurritako_liburuen_lagunak, current_page_zure_lag_lib=page_zure_lag_lib, total_pages_zure_lag_lib=total_pages_zure_lag_lib,
@@ -427,29 +431,62 @@ def info_liburu_catalogo(bookId):
    
     
     
-
-@app.route('/nireLagunak')
+# ***************************************************        Lagunak        ***************************************************
+@app.route('/nireLagunak', methods=['GET', 'POST'])
 def nireLagunak():
-        if not ('user' in dir(request) and request.user and request.user.token):
-            return redirect("/")
-        page = int(request.values.get("page", 1))
-        eraId = request.values.get("eraId", "")
-        lagun_zerrenda = request.user.getLagunak()
-        lagunak = []
-        for User in lagun_zerrenda:
-            lagunak.extend(
-                lagun
+    if not ('user' in dir(request) and request.user and request.user.token):
+        return redirect("/")
+
+    page = int(request.values.get("page", 1))
+    eraId = request.values.get("eraId", "")
+    lagun_zerrenda = request.user.getLagunak()
+    lagunak = []
+    for User in lagun_zerrenda:
+        lagunak.extend(
+            lagun
                 for lagun in lagun_zerrenda
-            )
-        lagunak = list(set(lagunak))
-        return render_template('nireLagunak.html', eraId=eraId, lagunak=lagunak, current_page=page, max=max, min=min)
+        )
+    lagunak = list(set(lagunak))
+
+    if "ezabatu" in request.values:
+        User.lagunaEzabatu(request.values.get("mail"))
+        return render_template('nireLagunak.html', eraId=eraId, lagunak=lagunak)
     
     
-@app.route('/jasotakoEskaerak')
+@app.route('/jasotakoEskaerak', methods=['GET', 'POST'])
 def jasotakoEskaerak():
-    return render_template('jasotakoEskaerak.html')
+    if not ('user' in dir(request) and request.user and request.user.token):
+        return redirect("/")
+
+    eskaera_zerrenda = request.user.getJasotakoEskaerak()
+    eskaerak = []
+    for User in eskaera_zerrenda:
+        eskaerak.extend(
+            eskaera
+            for eskaera in eskaerak
+        )
+    eskaerak = list(set(eskaerak))
+
+    if "onartu" in request.values:
+        User.eskaeraOnartu(request.values.get("mail"))
+    elif "ezeztatu" in request.values:
+        User.eskaeraEzeztatu(request.values.get("mail"))
+    return render_template('jasotakoEskaerak.html', eskaerak=eskaerak)
     
     
-@app.route('/bidalitakoEskaerak')
+@app.route('/bidalitakoEskaerak', methods=['GET', 'POST'])
 def bidalitakoEskaerak():
-    return render_template('bidalitakoEskaerak.html')
+    if not ('user' in dir(request) and request.user and request.user.token):
+        return redirect("/")
+
+    eskaera_zerrenda = request.user.getBidalitakoEskaerak()
+    eskaerak = []
+    for User in eskaera_zerrenda:
+        eskaerak.extend(
+            eskaera
+            for eskaera in eskaerak
+        )
+    eskaerak = list(set(eskaerak))
+    if "ezabatu" in request.values:
+        User.eskaeraEzabatu(request.values.get("mail"))
+    return render_template('bidalitakoEskaerak.html', eskaerak=eskaerak)
